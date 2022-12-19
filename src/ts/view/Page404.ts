d6a3abebd56734c9dfbf60eb226a49bab6668eb7
Vladimir Kurganov <err0rfrost@gmail.com>
2022-12-19 16:41:38 +0600

    feat: add classes for Home and 404 pages

---
+++ src/ts/view/Page404.ts
@@ -0,0 +1,14 @@
+import { AbstractView } from "./AbstractView";
+
+export class Page404 extends AbstractView {
+  constructor() {
+    super();
+    this.setTitle("404 Not Found");
+  }
+
+  async getHtml() {
+    return `
+      <h1>404 - Page not found</h1>
+    `;
+  }
+}
