d2a60cfc972fce45e7d0d68e5cec5962ea43716d
Vladimir Kurganov <err0rfrost@gmail.com>
2022-12-20 16:22:18 +0600

    feat: rewrite router to work with parameters in QS

--- src/ts/router.ts
+++ src/ts/router.ts
@@ -1,35 +1,46 @@
-/* eslint-disable @typescript-eslint/no-floating-promises */
 /* eslint-disable @typescript-eslint/no-misused-promises */
-import { Router } from './types/interface';
+import { Router, RouterMatch } from './types/interface';
 import { Home } from './view/Home';
 import { Page404 } from './view/Page404';
+
+function pathToRegex(path: string) {
+    return new RegExp(`^${path.replace(/\//g, '\\/').replace(/:\w+/g, '(.+)')}$`);
+}
+
+function getParams(match: RouterMatch) {
+    const values = match.result ? match.result.slice(1) : [];
+    const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map((result) => result[1]);
+
+    return Object.fromEntries(keys.map((key, i) => [key, values[i]]));
+}
 
 async function router() {
     const routes: Router[] = [
         { path: '/', view: Home },
         { path: '/404', view: Page404 },
-    ];
+        { path: '/product/:type', view: Home },
+      ];
 
     const potentialMatches = routes.map((route) => ({
         route,
-        result: window.location.pathname === route.path,
+        result: window.location.pathname.match(<RegExp>(<unknown>pathToRegex(route.path))),
     }));
 
-    let match = potentialMatches.find((potentialMatch) => potentialMatch.result === true);
+    let match = potentialMatches.find((potentialMatch) => potentialMatch.result !== null);
 
     if (!match) {
         match = {
             route: routes[1],
-            result: true,
+            result: [window.location.pathname],
         };
     }
 
     // eslint-disable-next-line new-cap
-    const view = new match.route.view();
+    const view = new match.route.view(getParams(match));
     try {
-      (document.querySelector('.main') as HTMLElement).innerHTML = await view.getHtml();      
+        (document.querySelector('.main') as HTMLElement).innerHTML = await view.getHtml();
     } catch (error) {
-      (document.querySelector('.main') as HTMLElement).innerHTML = 'error';      
+        (document.querySelector('.main') as HTMLElement).innerHTML = 'error';
     }
 }
 
@@ -40,13 +51,13 @@ async function navigateTo(url: string) {
 
 window.addEventListener('popstate', router);
 
-document.addEventListener('DOMContentLoaded', () => {
-    document.body.addEventListener('click', (e) => {
+document.addEventListener('DOMContentLoaded', async () => {
+    document.body.addEventListener('click', async (e) => {
         if ((<HTMLElement>e.target).matches('[data-link]')) {
             e.preventDefault();
-            navigateTo((<HTMLAnchorElement>e.target).href);
+            await navigateTo((<HTMLAnchorElement>e.target).href);
         }
     });
 
-    router();
+    await router();
 });
