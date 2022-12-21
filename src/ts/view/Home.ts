7d9b18dd688349a2b48acdfabcc7de8042f0f0de
Walqer <32606809+Walqer@users.noreply.github.com>
2022-12-21 16:40:58 +0600

    feat: add cards style

--- src/ts/view/Home.ts
+++ src/ts/view/Home.ts
@@ -10,13 +10,18 @@ export class Home extends AbstractView {
 
     async getHtml() {
         return `
-      <h1>Home</h1>
+      <h1 class="main-title visually-hidden">Home</h1>
+        <section class="products-wrapper">
+            <aside class="sidebar"></aside>
+            <ul class="products-list">
+        </ul>
+      </section>
     `;
     }
 
     async mounted() {
         const products = (await loadProducts()) as unknown as IProducts;
-        const page = document.querySelector('#main-content');
+        const page = document.querySelector('.products-list');
         for (let item in products) {
             const card = new ProductCard(products[item]);
 
