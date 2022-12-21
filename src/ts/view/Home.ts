033dd742dce862ad39bf96cda648bd662702c9ad
Vladimir Kurganov <err0rfrost@gmail.com>
2022-12-22 02:18:39 +0600

    feat: add utility bar for products

--- src/ts/view/Home.ts
+++ src/ts/view/Home.ts
@@ -4,7 +4,6 @@ import { ProductCard } from '../service/StoreService';
 import { loadProducts, mount } from '../helpers/generate-cards';
 import { IProducts } from '../types/interface';
 
-
 export class Home extends AbstractView {
     constructor(params: QueryStringParams) {
         super(params);
@@ -16,8 +15,28 @@ export class Home extends AbstractView {
       <h1 class="main-title visually-hidden">Home</h1>
         <section class="products-wrapper">
             <aside class="sidebar"></aside>
-            <ul class="products-list">
-        </ul>
+            <div class="products">
+                <div class="products-utilities">
+                    <div class="products-count">100 Results</div>
+                    <div class="utilities">
+                        <div class="sort-bar">
+                            <span>Sort:</span>
+                            <select class="product-sort">
+                                <option value="1" selected="selected">Default</option>
+                                <option value="2">Price ASC</option>
+                                <option value="3">Price DESC</option>
+                            </select>
+                        </div>
+                        <div class="search-bar">
+                            <span>Search:</span>
+                            <input type="search" class="product-search">
+                        </div>
+
+                        </div>
+                </div>
+                <ul class="products-list">
+                </ul>
+            </div>
       </section>
     `;
     }
