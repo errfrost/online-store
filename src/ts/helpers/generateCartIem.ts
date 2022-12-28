95522cc59d281dc4e7f06cae224d8e25afb20d00
Alimossim <agaev.alimosim@gmail.com>
2022-12-29 00:12:03 +0600

    feat: add products to cart page

---
+++ src/ts/helpers/generateCartIem.ts
@@ -0,0 +1,40 @@
+import { IProductCard } from '../types/interface';
+export function generateCartItem(product: IProductCard) {
+    let productCount = 1;
+    return `
+    <li class="cart-item">
+        <span class="cart-item__number">1</span>
+        <img width="200" src="${product.thumbnail}" alt="${product.title}" height="120" class="cart-item__img">
+        <div class="cart-item__info">
+            <span class="cart-item__info-title">
+                ${product.title}
+            </span>
+            <hr class="cart-item__info-line">
+            <p class="cart-item__info-descr">
+                ${product.description}
+            </p>
+            <div class="cart-item__info-btn-wrapper">
+            <span class="cart-item__info-rating cart-item__info-button">
+                Rating: <b class="rating-count">
+                    ${product.rating}
+                </b>
+            </span>
+            <span class="cart-item__info-discount cart-item__info-button">
+                Discount: <b class="discount-count">
+                    ${product.discountPercentage}
+                </b>
+            </span>
+            </div>
+        </div>
+        <div class="cart-item__stock">
+            Stock: <b class="stock-count"> ${product.stock}</b>
+            <div class="stock-buttons-wrapper">
+                <button class="button button_plus">+</button>
+                <b class="cart__paginator-item">${productCount}</b>
+                <button class="button button_minus">-</button>
+            </div>
+            <span class="cart-item__total">${product.price * productCount }$</span>
+        </div>
+    </li>
+    `;
+}
--- src/ts/view/CartPage.ts
+++ src/ts/view/CartPage.ts
@@ -1,5 +1,10 @@
 import { AbstractView } from './AbstractView';
 import { QueryStringParams } from '../types/type';
+import { Cart } from '../service/Cart';
+import { generateCard, loadProducts } from '../helpers/generate-cards';
+import { IProducts } from '../types/interface';
+import { cartSum } from '../helpers/addProduct';
+import { generateCartItem } from '../helpers/generateCartIem';
 
 export class CartPage extends AbstractView {
     constructor(params: QueryStringParams) {
@@ -13,47 +18,20 @@ export class CartPage extends AbstractView {
       <section class="cart-section">
         <div class="cart">
             <div class="cart__topbar">
-                <span class="cart__title">PRODUCTS IN CART<span>
+                <span class="cart__title">PRODUCTS IN CART</span>
                 <span class="cart__items">
                     ITEMS:
                     <b class="cart__items-counter">3</b>
-                <span>
+                </span>
                 <span class="cart__paginator">
                     PAGE:
                     <button class="button button_prev"><</button>
                     <b class="cart__paginator-page">1</b>
                     <button class="button button_next">></button>
-                <span>
+                </span>
             </div>
             <ol class="cart__list">
-                <li class="cart-item">
-                    <img class="cart-item__img">
-                    <div class="cart-item__info>
-                        <span class="cart-item__info-title">
-                            MacBook Pro
-                        </span>
-                        <hr>
-                        <p class="cart-item__info-descr">
-                            An apple mobile which is nothing like apple
-                        </p>
-                        <span class="cart-item__info-rating">
-                            Rating: <b class="rating-count">4.57</b>
-                        </span>
-                        <span class="cart-item__info-rating">
-                            Discount: <b class="rating-count">12.96%</b>
-                        </span>
-                    <div>
-
-                                                    <div class="cart-item__count>
-                        <span class="cart-item__stock">
-                            Stock: <b class="stock=count">94</b>
-                            <button class="button button_plus"><</button>
-                            <b class="cart__paginator-page">1</b>
-                            <button class="button button_minus">></button>
-                            <span class="cart-item__total">1749$</span>
-                        </span>
-                    </div>
-                </li>
+                
             </ol>
         </div>
 
@@ -62,14 +40,31 @@ export class CartPage extends AbstractView {
                 <span class="summary__title">Summary<span>
             </div>
             <div class="summary__products">
-                Products: <span class="summary__products-counter">6</span>
-                Total: <span class="summary__products-counter">1749$</span>
-                <input type="text" placeholder="Enter promo code">
+                <div class="summary__products-title">
+                    Products: <span class="summary__products-counter">1</span>
+                </div>
+                <div class="summary__products-title">
+                    Total: <span class="summary__products-price">500$</span>
+                </div>
+                <input class="summary__products-promocode" type="text" placeholder="Enter promo code">
                 <button class="buy-btn">Buy now</button>
             </div>
         </div>
       </section>
     `;
     }
-    async mounted() {}
+    async mounted() {
+        const products = (await loadProducts()) as unknown as IProducts;
+        const cartCounter = document.querySelector('.cart-counter');
+        const cartTotal = document.querySelector('.cart-total__price');
+        const cartList = document.querySelector('.cart__list');
+        const shopCart = new Cart();
+        cartTotal!.textContent = `${cartSum(products, shopCart.show())}$`; ///Нужно как-то иначе сделать чтобы не дублировалось  думаю кака то функця обновлния днных
+        cartCounter!.textContent = `${shopCart.length()}`;
+        
+        for(let key of  shopCart.show()){
+          const item =   generateCartItem(products[key]);
+          cartList!.insertAdjacentHTML('beforeend',item);
+        }
+}
 }
